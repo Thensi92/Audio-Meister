c129cddf19c578b4781df82adbc2b8a3eb1964f0
Dan <djimemoy@everis.com>
2019-04-11 17:48:00 +0200

    Creada clase User y definido sus métodos

---
+++ app/models/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+
+    class User{
+
+        public function getDatos($user, $passwd){
+            //DEVOLVER DATOS DE USUARIO
+        }
+
+        public function modificarUsuario($user, $passwd, $arrayNuevosDatos){
+            //EDITAR DATOS DE USUARIO. COMPARAR LOS NUEVOS DATOS CON LOS DE SESION
+
+        }
+
+        public function eliminarUsuario($user, $passwd){
+            //ELIMINAR USUARIO
+        }
+    }
+?>
