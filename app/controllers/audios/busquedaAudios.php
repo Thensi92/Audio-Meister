9d9e37f95afc877db37d965828e97148ae57ac42
Thensi92 <warangelal@gmail.com>
2019-05-02 20:19:43 +0200

    Modificacion lectura tablas de busquedaAudios.php

--- app/controllers/audios/busquedaAudios.php
+++ app/controllers/audios/busquedaAudios.php
@@ -1,19 +1,22 @@
 <?php
 require "app/db/Pagination.php";
+
 /* Config Connection */
 $root = 'root';
 $password = '';
 $host = 'localhost';
 $dbname = 'audio_meister';
+
 $connection = new PDO("mysql:host=$host;dbname=$dbname;", $root, $password);
 $pagination = new PDO_Pagination($connection);
+
 $search = null;
     if(isset($_REQUEST["search"]) && $_REQUEST["search"] != "") {
         $search = htmlspecialchars($_REQUEST["search"]);
         $pagination->param = "&search=$search";
-        $pagination->rowCount("SELECT * FROM productos WHERE titulo LIKE '%$search%' ");
+        $pagination->rowCount("SELECT * FROM audios WHERE nombre_audio LIKE '%$search%' ");
         $pagination->config(5, 4);
-        $sql = "SELECT * FROM productos WHERE titulo LIKE '%$search%' ORDER BY id ASC LIMIT $pagination->start_row, $pagination->max_rows";
+        $sql = "SELECT * FROM audios WHERE nombre_audio LIKE '%$search%' ORDER BY fecha_subida ASC LIMIT $pagination->start_row, $pagination->max_rows";
         $query = $connection->prepare($sql);
         $query->execute();
         $model = array();
@@ -21,9 +24,9 @@ $search = null;
             $model[] = $rows;
         }
     }else {
-        $pagination->rowCount("SELECT * FROM productos");
+        $pagination->rowCount("SELECT * FROM audios");
         $pagination->config(5, 4);
-        $sql = "SELECT * FROM productos ORDER BY id ASC LIMIT $pagination->start_row, $pagination->max_rows";
+        $sql = "SELECT * FROM audios ORDER BY fecha_subida ASC LIMIT $pagination->start_row, $pagination->max_rows";
         $query = $connection->prepare($sql);
         $query->execute();
         $model = array();
