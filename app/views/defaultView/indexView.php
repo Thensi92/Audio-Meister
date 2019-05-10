f440dda154db9eff1e64ad395c6824aa0b52b0b1
Thensi92 <warangelal@gmail.com>
2019-05-10 11:41:25 +0200

    Añadido filtro de visualizacion por genero

--- app/views/defaultView/indexView.php
+++ app/views/defaultView/indexView.php
@@ -1,35 +1,54 @@
 <?php
-require_once('app/controllers/audios/busquedaAudios.php');
+require_once('app/controllers/audios/indexViewController.php');
 ?>
 
 <section>
 
-    <div id="view_product">
-            
-    <?php
-    foreach($model as $row) {
-        
-        echo '<div class="card" style="width: 18rem;">';
-        echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
-        
-        echo '<div class="card-body">';
-        echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
-        echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
-        echo '</div>';
-        
-        echo '</div>';
-    }
+<div id="view_product">
     
-    ?>   
-    </div>
-    
-    
-    <?php
-    echo "<center>";
-        echo "<div>";
-            $pagination->pages("btn btn-primary");
-        echo "</div>";
-    echo "</center>";
-    ?>
+<?php
+
+echo '<div>Podcast</div>';
+echo '<div id="podcast">';
+foreach($podcast as $row){
+    echo '<div class="card" style="width: 18rem;">';
+    echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
+                
+    echo '<div class="card-body">';
+    echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
+    echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
+    echo '</div>';
+                
+    echo '</div>';
+}
+echo '</div>';
+
+echo '<div>Musica</div>';
+echo '<div id="musica">';
+foreach($musica as $row){
+    echo '<div class="card" style="width: 18rem;">';
+    echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
+                
+    echo '<div class="card-body">';
+    echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
+    echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
+    echo '</div>';
+                
+    echo '</div>';
+}
+echo '</div>';
+
+?>   
+</div>
+
+<?php
+echo "<center>";
+    echo "<div>";
+        $pagination->pages("btn btn-primary");
+    echo "</div>";
+echo "</center>";
+
+
+?>
     
 </section>
