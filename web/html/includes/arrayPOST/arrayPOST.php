23e83ac4dae1d5718f8d8136c49337b65df61ddc
Dan <futurenoird@gmail.com>
2019-05-12 18:35:48 +0200

    Corregida ruta de los controladores

--- web/html/includes/arrayPOST/arrayPOST.php
+++ web/html/includes/arrayPOST/arrayPOST.php
@@ -3,7 +3,7 @@
 $arrayPOST = [
     'procesar_login' => "app/controllers/Usuarios/LoginController.php",
     'registrar_usuario' => "app/controllers/Usuarios/RegisterController.php",
-    'editando_usuario' => "app/controllers/Usuarios/EditUserController.php"
+    'editando_usuario' => "app/controllers/Usuarios/funciones_usuario/EditUserController.php"
 ];
 
 ?>
