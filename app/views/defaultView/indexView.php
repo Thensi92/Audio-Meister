55b6840aca5c35d9b29d74bb6546fd5346de5d04
Thensi92 <warangelal@gmail.com>
2019-05-13 10:47:48 +0200

    Añadido filtro automatico de genero en el index

--- app/views/defaultView/indexView.php
+++ app/views/defaultView/indexView.php
@@ -6,7 +6,7 @@ require_once('app/controllers/audios/indexViewController.php');
     <div id="view_product">
     
     <?php
-    if($genero_podcast == 1 && ) {
+    if(!empty($podcast)) {
         echo '<div>Podcast</div>';
     }
 
@@ -24,7 +24,25 @@ require_once('app/controllers/audios/indexViewController.php');
     }
     echo '</div>';
 
-    if($genero_podcast == 1) {
+    if(!empty($voz)) {
+        echo '<div>Voz</div>';
+    }
+
+    echo '<div id="voz">';
+    foreach($musica as $row){
+        echo '<div class="card" style="width: 18rem;">';
+        echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
+                    
+        echo '<div class="card-body">';
+        echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
+        echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
+        echo '</div>';
+                    
+        echo '</div>';
+    }
+    echo '</div>';
+
+    if(!empty($musica)) {
         echo '<div>Musica</div>';
     }
 
