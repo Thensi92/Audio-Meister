dd80a30b0aa515af9d6ccdcedf65a12810918e0e
Thensi92 <warangelal@gmail.com>
2019-05-16 15:48:14 +0200

    Mejorado aspecto visual del error al logear

--- app/views/errores/errorLogin.php
+++ app/views/errores/errorLogin.php
@@ -1,14 +1,9 @@
-<section>
+<div id="mensajeError">
+    <div class="card">
+        <div class="card-body">
+            Upps...El usuario o contraseña son incorrectos
+        </div>
 
-    <div id="errorLogin">
-        <p> El correo o contraseña introducidos son incorrectos. </p>
-    <br>
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?ctl=login">Login</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="index.php?ctl=registrar">Registrar</a>
-        </li>
-
-</section>
+        <a id="botonError" href="index.php" class="btn btn-primary">Home</a>
+    </div>
+</div>
