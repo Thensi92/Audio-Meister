b0a2946fc06c667ff64acc01f718ba9561218ea7
Thensi92 <warangelal@gmail.com>
2019-05-16 03:07:56 +0200

    Añadido mensajes de error para los filtros

--- app/views/errores/vista_podcastVacio.php
+++ app/views/errores/vista_podcastVacio.php
@@ -1,5 +1,22 @@
-<section>
-    <div id="arrayVacio">
-        <p>Error...Se te va a redirigir al home </p>
+<section id="">
+    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div id="modal_button" class="modal-body">
+                    <a href="index.php"><button type="button" class="btn btn-info">Home</button></a>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <div id="errorPodcast" class="card">
+        <div class="card-body">
+            This is some text within a card body.
+        </div>
+
+        <button id="arrayVacio" type="button" class="btn btn-primary" data-toggle="modal" data-target="#miModal">
+            Atras
+        </button>
+    </div>
+
 </section>
