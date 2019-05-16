e74a541a88af68f42432d4cf69001b56fee2452c
Thensi92 <warangelal@gmail.com>
2019-05-16 15:03:16 +0200

    Mejorado aspecto visual musica por filtro

--- app/views/filters/vozFilter.php
+++ app/views/filters/vozFilter.php
@@ -3,21 +3,20 @@
     <?php
     if(!empty($voz)) {
         echo '<h1 class="titulos_generos">NOTAS DE AUDIO</h1>';
-    }
-
-    echo '<div id="podcast" class="posicionamientoFlexible">';
-    foreach($voz as $row){
-        echo '<div class="card" style="width: 18rem;">';
-        echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
-                    
-        echo '<div class="card-body">';
-        echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
-        echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
+            
+        echo '<div id="podcast" class="posicionamientoFlexible">';
+        foreach($voz as $row){
+            echo '<div class="card" style="width: 18rem;">';
+            echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
+            
+            echo '<div class="card-body">';
+            echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
+            echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
+            echo '</div>';
+            
+            echo '</div>';
+        }
         echo '</div>';
-                    
-        echo '</div>';
-    }
-    echo '</div>';
     ?>   
     </div>
 
@@ -27,4 +26,8 @@
             $pagination->pages("btn btn-primary");
         echo "</div>";
     echo "</center>";
+    
+    }else{
+        require_once("app/views/errores/vista_podcastVacio.php");
+    }
     ?>
