6b173297e6e4086d4337d9008ef17aafd05c177c
Thensi92 <warangelal@gmail.com>
2019-05-16 03:21:41 +0200

    Mejorado css para los mensajes de error

--- app/views/errores/vista_podcastVacio.php
+++ app/views/errores/vista_podcastVacio.php
@@ -1,22 +1,11 @@
 <section id="">
-    <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div id="modal_button" class="modal-body">
-                    <a href="index.php"><button type="button" class="btn btn-info">Home</button></a>
-                </div>
+    <div id="mensajeError">
+        <div class="card">
+            <div class="card-body">
+                Upps...No hay elementos del genero que buscas
             </div>
+
+            <a id="botonError" href="index.php" class="btn btn-primary">Home</a>
         </div>
     </div>
-
-    <div id="errorPodcast" class="card">
-        <div class="card-body">
-            This is some text within a card body.
-        </div>
-
-        <button id="arrayVacio" type="button" class="btn btn-primary" data-toggle="modal" data-target="#miModal">
-            Atras
-        </button>
-    </div>
-
 </section>
