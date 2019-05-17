c352668eada7b561febdc60d8aecb028c0eae932
Thensi92 <warangelal@gmail.com>
2019-05-18 00:47:37 +0200

    Añadida mejora css que posiciona el footer 1920x1080

--- web/html/body/footer/footer.php
+++ web/html/body/footer/footer.php
@@ -1,14 +1,15 @@
-<footer class="text-muted pag">
+<footer id="footer" class="text-muted pag">
 <div class="card text-center">
   <div class="card-footer text-muted">
   <button class="btn btn-dark btn-lg pull-xs-right">
-            <a href="index.php"> Bac to top </a>
+            <a href="index.php"> Back to the future </a>
         </button>
-        <p><a href="#">ClayDesk</a> 2019</p>
-        <p> Coded by  </p>
+        <p><a href="#">Tri</a> 2019</p>
+        <p> Coded by Tri </p>
   </div>
 </div>
 </footer>
+</div>
 
 </body>
 </html>
