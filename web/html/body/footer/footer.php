0d19af6f0ab2c08ce7cd07b477e3dcbb79c349c0
Thensi92 <warangelal@gmail.com>
2019-05-27 17:16:04 +0200

    Añadida nueva id al footer para aspecto visual

--- web/html/body/footer/footer.php
+++ web/html/body/footer/footer.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="text-muted pag">
-<div class="card text-center">
+<div id="fondo-footer" class="card text-center">
   <div class="card-footer text-muted">
   <button class="btn btn-dark btn-lg pull-xs-right">
             <a href="index.php"> Back to the future </a>
