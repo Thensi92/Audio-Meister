c7553a33e6735cf7510a8ce069176e104953b7a4
soronsiojamba <somardero@gmail.com>
2019-05-30 15:41:02 +0200

    Añadido apodo en comentario

--- app/controllers/audio/controller_detalleAudio.php
+++ app/controllers/audio/controller_detalleAudio.php
@@ -2,14 +2,22 @@
     include "app/db/config.php";
     include "app/models/conexion/model_conexion_BBDD.php";
     include "app/models/model_audio.php";
+    include "app/models/usuario/Usuario.php";
+
     $idAudioDetalle = $_GET["id"];
     $metConexion = Config::$metodoConexion;
     $user= Config::$usuario;
     $contra = Config::$contraseña;
     $nombreBD = Config::$nombreBaseDatos;
+
     $conexion = new Audio($metConexion,$user,$contra,$nombreBD);
     $arrayDatos = $conexion->buscarAudio($idAudioDetalle);
     $conexion->cerrarConexion();
+
+    $conexionApodo = new Usuario($metConexion,$user,$contra,$nombreBD);
+    $correoEjem = $arrayDatos["correoUsuario"];
+    $apodo = $conexionApodo->getApodo($correoEjem);
+
     //Vista de detalle con el reproductor
     include "app/views/audio/viewDetalleAudio.php";
 ?>
