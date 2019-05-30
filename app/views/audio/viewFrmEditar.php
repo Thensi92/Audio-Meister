3e421855362df676e9809a7414cc1fc575db78d0
Thensi92 <warangelal@gmail.com>
2019-05-30 15:25:39 +0200

    Añadido bloque css para editar audio

--- app/views/audio/viewFrmEditar.php
+++ app/views/audio/viewFrmEditar.php
@@ -1,27 +1,28 @@
-  
-    <script>
-            function mostrar(){
-                obj = document.getElementById("archivo");
-                obj.style.visibility = (obj.style.visibility == 'hidden') ? 'visible' : 'hidden';
-            }
-    </script>
-   <form id="form" action="index.php?ctl=editar_audio&id=<?=$arrayDatos["id"]?>" method="POST" enctype="multipart/form-data">
-        <div class="form-group">
-                    <label for="nombreAudio">Nombre Audio</label>
-                    <input type="text" class="form-control" id="nombreAudio" name="nombreAudio" value="<?=$arrayDatos["nombre"]?>" required>
-                </div>
+<script>
+    function mostrar() {
+        obj = document.getElementById("archivo");
+        obj.style.visibility = (obj.style.visibility == 'hidden') ? 'visible' : 'hidden';
+    }
+</script>
+<form id="form" action="index.php?ctl=editar_audio&id=<?=$arrayDatos["id"]?>" method="POST"
+    enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="nombreAudio">Nombre Audio</label>
+        <input type="text" class="form-control" id="nombreAudio" name="nombreAudio" value="<?=$arrayDatos["nombre"]?>"
+            required>
+    </div>
 
-                <b>Cambiar Audio</b>
-                <input type="checkbox" name="check" id="check" value="1" onchange="mostrar()" />
+    <b>Cambiar Audio</b>
+    <input type="checkbox" name="check" id="check" value="1" onchange="mostrar()" />
 
-                <div id="archivo" class="form-group" style="visibility:hidden">
-                    <input type="file" id="archivoInput" name="archivoAudio" id="archivo">
-                </div>
+    <div id="archivo" class="form-group" style="visibility:hidden">
+        <input type="file" id="archivoInput" name="archivoAudio" id="archivo">
+    </div>
 
-                <div clas="form-control">
-                    <label for="tipoAudio">Tipo Audio</label>
-                    <select class="custom-select" id="tipoAudio" name="tipoAudio" required>
-                        <?php
+    <div clas="form-control">
+        <label for="tipoAudio">Tipo Audio</label>
+        <select class="custom-select" id="tipoAudio" name="tipoAudio" required>
+            <?php
                             $tipoAudio = $arrayDatos["tipo"];
                             if($tipoAudio == "1"){
                                 echo '<option value="1" selected>Podcast</option>
@@ -36,13 +37,13 @@
                                         <option value="2">Nota Audio</option>
                                         <option value="3" selected>Musica</option>';
                             }
-                        ?> 
-                    </select>
+                        ?>
+        </select>
 
-                     <div class="form-group">
-                        <label for="visibilidad">Visibilidad</label>
-                            <select class="custom-select" id="visibilidad" name="visibilidad">
-                                <?php
+        <div class="form-group">
+            <label for="visibilidad">Visibilidad</label>
+            <select class="custom-select" id="visibilidad" name="visibilidad">
+                <?php
                                     $visibilidad = $arrayDatos["visibilidad"];
                                     if($visibilidad == "publico"){ 
                                         echo '<option value="publico" selected>Publico</option>
@@ -52,13 +53,13 @@
                                               <option value="privado" selected>Privado</option>';
                                     }
                                 ?>
-                            </select>
-                      </div>
+            </select>
+        </div>
 
-                      <div class="form-group">
-                            <label>Imagen</label>
-                            <select class="custom-select" id="imagen" name="imagen">
-                            <?php
+        <div class="form-group">
+            <label>Imagen</label>
+            <select class="custom-select" id="imagen" name="imagen">
+                <?php
                                 $rutaImagen = $arrayDatos["rutaImagen"];
 
                                 if($rutaImagen == "web/html/body/img/Poster1.png"){
@@ -80,8 +81,13 @@
 
                                 }
                             ?>
-                            </select>
-                      </div>
-                    <button type="submit" name="editar_audio" class="btn btn-primary">Editar</button>
-                </div>
-                <a class="btn btn-danger" href="index.php?ctl=verMsg&ctlSecundario=eliminarAudio" role="button">Borrar</a>
+            </select>
+        </div>
+    </div>
+
+    </div id="opciones-edicion">
+        <button type="submit" name="editar_audio" class="btn btn-primary">Editar</button>
+        <a class="btn btn-danger" href="index.php?ctl=verMsg&ctlSecundario=eliminarAudio" role="button">Borrar</a>
+    </div>
+
+</form>
