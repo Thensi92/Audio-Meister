ac7c37a903f894cf9eaca85f4e833eaf0a941dea
Thensi92 <warangelal@gmail.com>
2019-05-30 13:50:16 +0200

    Añadido css para los comentarios

--- app/controllers/comentario/controllerViewFrmComentario.php
+++ app/controllers/comentario/controllerViewFrmComentario.php
@@ -1,6 +1,3 @@
 <?php
-    $usuario = $_SESSION["datosUser"]["correo"];
-    $idAudio = $_GET["id"];
-
     include "app/views/comentario/viewFrmComentario.php";
 ?>
--- app/views/comentario/viewFrmComentario.php
+++ app/views/comentario/viewFrmComentario.php
@@ -1,11 +1,17 @@
 <?php
- echo '<form id="subida-comentarios" action="index.php?ctl=up&idAudio='.$idAudio.'&correo='.$usuario.' " method="post" >';
- ?>
-        <div class="form-group">
-        <label for="comment">Escribe el comentario:</label>
-        <textarea class="form-control" rows="3" cols="95"  name="comentario" required></textarea>
-        </div>
+if(!empty($_SESSION{'datosUser'})){
+    
+    $usuario = $_SESSION["datosUser"]["correo"];
+    $idAudio = $_GET["id"];
+
+    echo '<form id="subida-comentarios" action="index.php?ctl=up&idAudio='.$idAudio.'&correo='.$usuario.' " method="post" >';
+        echo "<div class=form-group>";
+        echo "<label for=comment>Escribe el comentario:</label>";
+        echo "<textarea class=form-control rows=3 cols=95  name=comentario required></textarea>";
+        echo "</div>";
 
         
-    <button type="submit" name="subirComentario" class="btn btn-primary">Subir</button>
-</form>
+    echo "<button type=submit name=subirComentario class='btn btn-primary'>Subir</button>";
+    echo "</form>";
+}
+    ?>
