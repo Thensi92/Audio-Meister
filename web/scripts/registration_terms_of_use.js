10b13af6f8c82de2d4f91cd733fe23669ad502c9
Dan <futurenoird@gmail.com>
2019-06-02 15:09:23 +0200

    Creado checkbox para aceptar los términos de uso

---
+++ web/scripts/registration_terms_of_use.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+    let checkboxTermsOfUse = document.getElementById("checkTermsOfUse");
+    let submitRegister = document.getElementById("submit");
+
+    checkboxTermsOfUse.addEventListener("click", isChecked);
+
+    function isChecked() {
+        if (this.checked) {
+            submitRegister.disabled = false;
+        } else {
+            submitRegister.disabled = true;
+        }
+    }
+}
+
+
+
+
+
