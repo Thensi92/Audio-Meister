9245c1072d0ac00f570bc11fcfd8450d0ba8cfa0
Dan <futurenoird@gmail.com>
2019-06-03 21:15:01 +0200

    Mejoras en la legibilidad el código

--- app/controllers/audio/controllerSubidaAudio.php
+++ app/controllers/audio/controllerSubidaAudio.php
@@ -4,24 +4,28 @@
     include "app/models/model_audio.php";
     include "app/lib/getid3/getid3.php";
 
-    $metConexion = Config::$metodoConexion;
-    $user= Config::$usuario;
-    $contra = Config::$contraseña;
-    $nombreBD = Config::$nombreBaseDatos;
+    $metConexion    =  Config::$metodoConexion;
+    $user           =  Config::$usuario;
+    $contra         =  Config::$contraseña;
+    $nombreBD       =  Config::$nombreBaseDatos;
 
-    $conexion = new Audio($metConexion,$user,$contra,$nombreBD);
+    $conexion       =  new Audio($metConexion,$user,$contra,$nombreBD);
     //$conexionCount = new Audio($metConexion,$user,$contra,$nombreBD);
 
-    $nombre=$_POST["nombreAudio"];
-    $formatoArchivo = $_FILES["archivoAudio"]["type"];
-    $tamañoArchivo = $_FILES["archivoAudio"]["size"];
-    $rutaTemporal = $_FILES["archivoAudio"]["tmp_name"];
-    $correoUser = $_SESSION["datosUser"]["correo"];
-    $tipoArchivo = $_POST["tipoAudio"];
-    $nArchivo= $_FILES['archivoAudio']['name'];
-    $visibilidad = (int)$_POST["visibilidad"];
-    $imagen = $_POST["imagen"];
+    $nombre         =   $_POST["nombreAudio"];
+    $formatoArchivo =   $_FILES["archivoAudio"]["type"];
+    $tamañoArchivo  =   $_FILES["archivoAudio"]["size"];
+    $rutaTemporal   =   $_FILES["archivoAudio"]["tmp_name"];
+    $correoUser     =   $_SESSION["datosUser"]["correo"];
+    $tipoArchivo    =   $_POST["tipoAudio"];
+    $nArchivo       =   $_FILES['archivoAudio']['name'];
+    $visibilidad    =   (int)$_POST["visibilidad"];
 
+    if(isset($_POST["audio"]["userImage"];)){
+        $imagen  =   $_POST["audio"]["userImage"];
+    }else{
+        $imagen  =   $_POST["imagen"];
+    }
    
     $resultado = $conexion->subirArchivoAudio($nombre,$nArchivo,$rutaTemporal,$formatoArchivo,$tamañoArchivo,$correoUser,$tipoArchivo,$imagen,$visibilidad);
     $conexion->cerrarConexion();
--- app/models/model_audio.php
+++ app/models/model_audio.php
@@ -1,15 +1,19 @@
 <?php
     class Audio extends Conexion{
 
-        public function subirArchivoAudio($nombre,$nArchivo,$rutaTemporal,$formato,$tamanio,$correoUser,$tipo, $rutaImagen , $visibilidad){
-            $tipoDefecto = "audio/mp3";
-            $tipoDefecto2 = "audio/mpeg";
-            $rutaConst = "web/musica/".$correoUser."/";
+        public function subirArchivoAudio(
+            $nombre, $nArchivo, $rutaTemporal, 
+            $formato, $tamanio, $correoUser, 
+            $tipo, $rutaImagen, $visibilidad){
+           
+            $tipoDefecto    = "audio/mp3";
+            $tipoDefecto2   = "audio/mpeg";
+            $rutaConst      = "web/musica/".$correoUser."/";
             $rutaDefinitiva = $rutaConst.$nArchivo;
-            $tamanioMaximo = 16777216;
+            $tamanioMaximo  = 16777216;
             
-            $tipoCasteado = (int)$tipo;
-            $codigo = rand(1,1000);
+            $tipoCasteado   = (int)$tipo;
+            $codigo         = rand(1,1000);
 
             if(file_exists($rutaDefinitiva)){
                 copy($rutaDefinitiva,$rutaConst.$codigo.$nArchivo);
@@ -19,23 +23,27 @@
             if($formato == $tipoDefecto || $formato == $tipoDefecto2){
                     if($tamanio > $tamanioMaximo){
                         $mensaje = "Has superado el tamaño permitido"; 
-                        $caja = $this->hacerMensajeError($mensaje);
+                        $caja    = $this->hacerMensajeError($mensaje);
+                        
                     }else{
-
                         if(move_uploaded_file($rutaTemporal,$rutaDefinitiva)){
 
                             //Llamas a la libreria para sacar la duracion de la cancion
-                            $filename =$rutaDefinitiva;
-                            $getID3 = new getID3;
-                            $file = $getID3->analyze($filename); 
+                            $filename         = $rutaDefinitiva;
+                            $getID3           = new getID3;
+                            $file             = $getID3->analyze($filename); 
                             $playtime_seconds = $file['playtime_seconds'];
-                            $duracion = gmdate("H:i:s",$playtime_seconds);
+                            $duracion         = gmdate("H:i:s",$playtime_seconds);
         
-                            $sql ="INSERT INTO audios (nombre_audio, ruta_audio, ruta_imagen_audio, duracion, correo, id_tipo, visibilidad) VALUES (?,?,?,?,?,?,?)";
+                            $sql = "INSERT INTO audios (nombre_audio, ruta_audio, ruta_imagen_audio, duracion, correo, id_tipo, visibilidad) VALUES (?,?,?,?,?,?,?)";
                             
                             $resultado = $this->conexion->prepare($sql);
-                            $resultado->bind_param("sssssii",$nombre,$rutaDefinitiva,$rutaImagen,$duracion,$correoUser,$tipoCasteado,$visibilidad);
+                            $resultado->bind_param("sssssii", 
+                            $nombre, $rutaDefinitiva, $rutaImagen, 
+                            $duracion, $correoUser, $tipoCasteado, $visibilidad);
+
                             $resultado->execute();
+
                             if($resultado->affected_rows == 1){
                                 $mensaje = "Exito"; 
                             }else{
@@ -53,7 +61,7 @@
         }
 
         public function getOwnAudios($correoUser){
-            $sql = "SELECT id_audio FROM audios WHERE correo = ?";
+            $sql       = "SELECT id_audio FROM audios WHERE correo = ?";
             $resultado = $this->conexion->prepare($sql);
             $resultado->bind_param("s",$correoUser);
             $resultado->execute();
@@ -68,7 +76,7 @@
         }
 
         public function buscarAudio($id){
-            $sql = "SELECT * FROM audios WHERE id_audio = ?";
+            $sql       = "SELECT * FROM audios WHERE id_audio = ?";
             $resultado = $this->conexion->prepare($sql);
             $resultado->bind_param("i",$id);
             $resultado->execute();
@@ -76,25 +84,26 @@
             $resultado->fetch();
 
             $datosAudio = [
-                'id' => $id,
-                'nombre' => $nombre,
-                'rutaAudio' => $ruta,
-                'rutaImagen' => $rutaImagen,
-                'duracion' => $duracion,
-                'fechaSubida' => $fechaSubida,
+                'id'            => $id,
+                'nombre'        => $nombre,
+                'rutaAudio'     => $ruta,
+                'rutaImagen'    => $rutaImagen,
+                'duracion'      => $duracion,
+                'fechaSubida'   => $fechaSubida,
                 'correoUsuario' => $correoUser,
-                'tipo' => $tipo,
-                'visibilidad' => $visibilidad
+                'tipo'          => $tipo,
+                'visibilidad'   => $visibilidad
             ];
 
             return $datosAudio;
         }
 
         public function borrarAudio($id,$correoUser,$url){
-            $sql = "DELETE FROM audios WHERE id_audio = ? AND correo= ?";
+            $sql       = "DELETE FROM audios WHERE id_audio = ? AND correo= ?";
             $resultado = $this->conexion->prepare($sql);
             $resultado->bind_param("is",$id,$correoUser);
             $resultado->execute();
+
             if($resultado->affected_rows == 1){
                 if(unlink($url)){
                     $mensaje = "Exito";
@@ -108,11 +117,11 @@
         }
 
         public function editarAudio($id,$nombre,$correoUser,$tipo,$modificarAudio,$nArchivo,$rutaTemporal,$formato,$tamanio,$rutaArchivoAntiguo,$rutaImagen,$visibilidad){
-            $tipoDefecto = "audio/mp3";
-            $rutaConst = "web/musica/".$correoUser."/";
+            $tipoDefecto    = "audio/mp3";
+            $rutaConst      = "web/musica/".$correoUser."/";
             $rutaDefinitiva = $rutaConst.$nArchivo;
-            $tamanioMaximo = 16777216;
-            $tipoCasteado = (int)$tipo;
+            $tamanioMaximo  = 16777216;
+            $tipoCasteado   = (int)$tipo;
 
             if($modificarAudio)
             {  
@@ -125,34 +134,31 @@
                     }else{
                             if(move_uploaded_file($rutaTemporal,$rutaDefinitiva)){
                                  //Llamas a la libreria para sacar la duracion de la cancion
-                                 $filename =$rutaDefinitiva;
-                                 $getID3 = new getID3;
-                                 $file = $getID3->analyze($filename); 
-                                 $playtime_seconds = $file['playtime_seconds'];
-                                 $duracion = gmdate("H:i:s",$playtime_seconds);
+                                $filename =$rutaDefinitiva;
+                                $getID3 = new getID3;
+                                $file = $getID3->analyze($filename); 
+                                $playtime_seconds = $file['playtime_seconds'];
+                                $duracion = gmdate("H:i:s",$playtime_seconds);
 
-                                 $sql ="UPDATE  audios SET nombre_audio = ? , ruta_audio = ?, ruta_imagen_audio = ?, duracion =? , id_tipo=? , visibilidad =? WHERE correo = ? AND id_audio = ?";
-                                 $resultado = $this->conexion->prepare($sql);
-                                 $resultado->bind_param("ssssiisi",$nombre,$rutaDefinitiva,$rutaImagen,$duracion,$tipoCasteado,$visibilidad,$correoUser,$id);
-                                 $resultado->execute();
+                                $sql ="UPDATE  audios SET nombre_audio = ? , ruta_audio = ?, ruta_imagen_audio = ?, duracion =? , id_tipo=? , visibilidad =? WHERE correo = ? AND id_audio = ?";
+                                $resultado = $this->conexion->prepare($sql);
+                                $resultado->bind_param("ssssiisi",$nombre,$rutaDefinitiva,$rutaImagen,$duracion,$tipoCasteado,$visibilidad,$correoUser,$id);
+                                $resultado->execute();
                                 
-                                 if($resultado->affected_rows == 1){
+                                if($resultado->affected_rows == 1){
                                     $mensaje = "Exito"; 
-                                 }else{
-                                   $mensaje = "Fallo en la subida del archivo"; 
-                                 }
-
-                                 
+                                }else{
+                                    $mensaje = "Fallo en la subida del archivo"; 
+                                }                               
                             }else{
                                 $mensaje = "Fallo al mover el fichero"; 
                             }
                     }
-
                 }else{
                     $mensaje = "No has subido un archivo mp3, o nada";
                 }
             }else{
-                $sql = "UPDATE audios SET nombre_audio = ? , id_tipo = ?, ruta_imagen_audio = ? , visibilidad = ?   WHERE correo = ? AND id_audio = ?";
+                $sql       = "UPDATE audios SET nombre_audio = ? , id_tipo = ?, ruta_imagen_audio = ? , visibilidad = ?   WHERE correo = ? AND id_audio = ?";
                 $resultado = $this->conexion->prepare($sql);
                 $resultado->bind_param("sisisi",$nombre,$tipoCasteado,$rutaImagen,$visibilidad,$correoUser,$id);
                 $resultado->execute();
@@ -167,7 +173,7 @@
         }
 
         public function borrarUsuario($correo){
-            $sql = "DELETE FROM usuarios WHERE correo = ?";
+            $sql       = "DELETE FROM usuarios WHERE correo = ?";
             $resultado = $this->conexion->prepare($sql);
             $resultado->bind_param("s",$correo);
             $resultado->execute();
