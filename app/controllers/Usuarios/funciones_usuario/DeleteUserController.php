2cb07fc27e7422fd5a234a31eccbcaf640347a40
Dan <futurenoird@gmail.com>
2019-06-04 00:13:59 +0200

    Reemplazada funcion eliminar_directorio. Ahora funciona correctamente.

--- app/controllers/Usuarios/funciones_usuario/DeleteUserController.php
+++ app/controllers/Usuarios/funciones_usuario/DeleteUserController.php
@@ -28,7 +28,7 @@
         //require_once("app/views/usuario/EliminarUsuarioView.php");
         echo $mensaje;
     }
-
+/*  
     function eliminar_directorio($dir){
         $directorio = opendir($dir);
         while ($archivo = readdir($directorio)){
@@ -43,5 +43,25 @@
         closedir($directorio);
         rmdir($dir);
     }
-
+*/
+    function eliminar_directorio($dir){
+        $result = false;
+        if ($handle = opendir("$dir")){
+            $result = true;
+            while ((($file=readdir($handle))!==false) && ($result)){
+                if ($file!='.' && $file!='..'){
+                    if (is_dir("$dir/$file")){
+                        $result = eliminar_directorio("$dir/$file");
+                    } else {
+                        $result = unlink("$dir/$file");
+                    }
+                }
+            }
+            closedir($handle);
+            if ($result){
+                $result = rmdir($dir);
+            }
+        }
+        return $result;
+    }
 ?>
