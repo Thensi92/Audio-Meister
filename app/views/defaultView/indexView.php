1f3ae2ddabfb496f0022b550c51b140610787be2
Thensi92 <warangelal@gmail.com>
2019-06-04 20:36:21 +0200

    Retoques css al reproductor

--- app/views/defaultView/indexView.php
+++ app/views/defaultView/indexView.php
@@ -7,10 +7,10 @@ require_once('app/controllers/audios/indexViewController.php');
     <?php
     echo '<div class="posicionamientoFlexible">';
     foreach($model as $row){
-        echo '<div class="card" style="width: 18rem;">';
+        echo '<div id="caja-tarjeta" class="card" style="width: 18rem;">';
         echo '<img class="card-img-top" src='.$row["ruta_imagen_audio"].'>';
                     
-        echo '<div class="card-body">';
+        echo '<div id="caja-body-tarjeta" class="card-body">';
         echo '<h5 class="card-title">'.$row["nombre_audio"].'</h5>';
         echo '<a href="index.php?ctl=detalleAudio&&id='.$row["id_audio"].'" class="btn btn-primary">Ver</a>';
         echo '</div>';
